a8d1cd5032510a5c03e12491add25629b8a31156
ErlanggaPuthen <11211057@student.itk.ac.id>
2026-08-14 14:40:30 +0800

    Switch Tailwind CSS

--- src/main.jsx
+++ src/main.jsx
@@ -1,16 +1,10 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
+import React from 'react'
+import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
-import Navbar from './components/Navbar.jsx'
+import './index.css'
 
-import 'remixicon/fonts/remixicon.css'
-
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <div className="container mx-auto px-4">
-      <Navbar />
-      <App />
-    </div>
-  </StrictMode>,
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
 )
